977ccb29614a78b4563c0ec7ecb7e62795551386
Biwash bohora <biwashbohora55@gmail.com>
2025-08-06 00:52:12 +0545

    Implement Login and Sign Up page layout with styling

--- src/pages/LoginSignUp.jsx
+++ src/pages/LoginSignUp.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 export const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <h6>Username: </h6>
+          <input type="text" placeholder='Your Name' />
+          <h6>Email: </h6>
+          <input type="email" placeholder='Your Email' />
+          <h6>Password: </h6>
+          <input type="password" placeholder='Password' />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login Here</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id='' />
+          <p>By continuing, I agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
