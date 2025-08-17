523ccc8a3ea6a2a926cfc5c28c8bdfebae4f31b7
Biwash bohora <biwashbohora55@gmail.com>
2025-08-17 11:45:22 +0545

    Add product display

--- src/components/ProductDisplay/ProductDisplay.jsx
+++ src/components/ProductDisplay/ProductDisplay.jsx
@@ -33,6 +33,32 @@ const ProductDisplay = (props) => {
             <img src={start_dull_icon} alt="" />
         <p>(122)</p>
         </div>
+        {/* PRICE OF PRODUCT */}
+        <div className="productDisplay-right-prices">
+            <div className="productDisplay-right-old-price">${product.old_price}</div>
+            <div className="productDisplay-right-new-price">${product.new_price}</div>
+        </div>
+        {/* DESCRIPTION OF PRODUCT*/}
+        <div className="productDisplay-right-description">
+            A rounded neck fullsleeve shirt with printed Deathnote anime.
+        </div>
+        {/* SIZE OF THE PRODUCT */}
+        <div className="productDisplay-right-size">
+            <h1>Select Size</h1>
+            <div className="productDisplay-right-size">
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
+                <div>XL</div>
+                <div>XXL</div>
+            </div>
+            <button>ADD TO CART</button>
+            {/* TAGS AND CATEGORY OF PRODUCT */}
+            <p className="productDisplay-right-category">
+                <span>Category :</span>Men, Shirt </p>
+            <p className="productDisplay-right-category">
+                <span>Tags :</span>moderate, latest </p>
+        </div>
     </div>
     </div>
   )
